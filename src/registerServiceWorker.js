b70d9072b63a59ae465402d5f5544f3ffbd54840
Martien Dermawan Tanama <martiendt@gmail.com>
2020-06-25 12:59:39 +0700

    let service worker update before refresh

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -23,7 +23,7 @@ if (process.env.NODE_ENV === 'production') {
       console.log('New content is available; please refresh.')
       setTimeout(() => {
         window.location.reload(true)
-      }, 1000)
+      }, 3000)
     },
     offline () {
       console.log('No internet connection found. App is running in offline mode.')
